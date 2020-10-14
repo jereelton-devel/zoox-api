780e4751d8539c60f6d40d9f83c3ab26a5d0d2c5
Jereelton Teixeira <jereelton@gmail.com>
2020-10-14 12:47:34 -0300

    config env mongo
    Initial configurations on mongoDB

--- index.php
+++ index.php
@@ -5,11 +5,67 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use \Slim\Slim;
+use \MongoDB\Client;
 
 $app = new Slim();
 
+date_default_timezone_set("America/Sao_Paulo");
+
+$headers = apache_request_headers();
+//$origin  = $headers['Origin'];
+/*echo '<pre>';
+var_dump($headers);
+echo '</pre>';*/
+
 $app->get('/', function(){
-	echo "ZOOX-API";
+	echo "<h1>ZOOX-API</h1>";
+
+    $client = new Client(
+        'mongodb://localhost:27017'
+    );
+
+    //Atualizar
+    /*$zooxupdate = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->updateOne(["id"=>1],['$set'=>["nome"=>"Amazonas"]]);
+
+    echo $zooxupdate->getModifiedCount();*/
+
+
+    //Contar Especifico
+    $zooxcount = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->countDocuments(["nome"=>"Rio de Janeiro"]);
+    //Contar Todos
+    $zooxcount = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->countDocuments();
+
+    //Listar
+    $zooxlist = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->find();
+
+    echo $zooxcount;
+
+    echo '<pre>';
+    foreach ($zooxlist as $document) {
+        echo $document["id"] .' - '. $document["nome"] . "\n";
+    }
+    echo '</pre>';
+
+
+
+    //Inserir
+    /*$zoox = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->insertOne(["id"=>3,"nome"=>"Rio de Janeiro","sigla"=>"RJ","data_criacao"=>"10/10/2020","data_atualizacao"=>"14/10/2020"]);*/
+
+
+
+    //Remover
+    //$zoox = $client->selectDatabase('zoox_mongodb')->selectCollection('zoox_mongodb_collection')->deleteOne(["id"=>4]);
+
+
+
+    /*echo '<pre>';
+    var_dump($zoox);
+    echo '</pre>';
+
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';*/
+
 });
 
 //$app->post();
